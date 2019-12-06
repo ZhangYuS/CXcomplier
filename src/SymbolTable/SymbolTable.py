0937329cf4ff32a27acf152e90b892f8a6838b68
SYRain <834544739@qq.com>
2019-12-06 12:09:16 +0800

    符号表添加函数

--- src/SymbolTable/SymbolTable.py
+++ src/SymbolTable/SymbolTable.py
@@ -23,3 +23,19 @@ class SymbolTable:
     def close_function_scope(self):
         self.current_scope, size = self.current_scope.close_function_scope()
         return size
+
+
+    def is_function_existed(self, function_name):
+        return function_name in self.function_symbol.keys()
+
+    def add_function_name(self, identifier, return_type, parameter_num, parameter_type_list):
+        if self.is_function_existed(identifier):
+            pass # TODO 函数已存在错误
+        else:
+            self.function_symbol[identifier] = FunctionSymbol(identifier, return_type, parameter_num, parameter_type_list)
+
+    def set_function_size(self, function_name, size):
+        self.function_symbol[function_name].size = size
+
+
+
