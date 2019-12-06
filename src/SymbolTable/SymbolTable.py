5b25ab9317c93ece4df04dcca8016abc1fb8052f
SYRain <834544739@qq.com>
2019-12-06 12:08:28 +0800

    区分普通作用域以及函数作用域

--- src/SymbolTable/SymbolTable.py
+++ src/SymbolTable/SymbolTable.py
@@ -1,17 +1,25 @@
 from .Scope import Scope
+from .Symbol import Symbol
+from .FunctionSymbol import FunctionSymbol
 
 
 class SymbolTable:
 
-    global_scope = None
-    current_scope = None
-
     def __init__(self):
         self.global_scope = Scope(None)
         self.current_scope = self.global_scope
+        self.function_symbol = dict()
+        self.type_code = {'int': 'i', 'bool': 'b', 'real': 'r'}
 
     def open_scope(self):
         self.current_scope = self.current_scope.open_scope()
 
     def close_scope(self):
-        self.current_scope = self.current_scope.get_father_scope()
+        self.current_scope = self.current_scope.close_scope()
+
+    def open_function_scope(self):
+        self.current_scope = self.current_scope.open_function_scope()
+
+    def close_function_scope(self):
+        self.current_scope, size = self.current_scope.close_function_scope()
+        return size
