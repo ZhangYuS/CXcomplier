558c358c555cdbe5757b4e4c148300c2ea4c960c
SYRain <834544739@qq.com>
2019-12-06 12:13:24 +0800

    完成函数声明时添加函数名到函数符号表里，添加函数参数到符号表中

--- src/SymbolTable/FunctionSymbol.py
+++ src/SymbolTable/FunctionSymbol.py
@@ -1,15 +1,8 @@
 class FunctionSymbol:
-    identifier = None
-    return_type = None
-    parameter_type_list = []
-    parameter_number = 0
-    address = 0
-    size = 0
 
-    def __init__(self, identifier, return_type, parameter_number, parameter_type_list, address, size):
+    def __init__(self, identifier, return_type, parameter_number, parameter_type_list, size=0):
         self.identifier = identifier
         self.return_type = return_type
         self.parameter_number = parameter_number
         self.parameter_type_list = parameter_type_list
-        self.address = address
         self.size = size
--- src/ast/ASTBuilder.py
+++ src/ast/ASTBuilder.py
@@ -1,13 +1,14 @@
 from resources.grammerParser import grammerParser
 from resources.grammerLexer import grammerLexer
+from src.SymbolTable.SymbolTable import SymbolTable
+
 
 class ASTBuilder:
-    current_function = None
-    function_size = 0
 
-    def __init__(self, tree: grammerParser.RContext, symbol_table):
+    def __init__(self, tree: grammerParser.RContext, symbol_table: SymbolTable):
         self.tree = tree
         self.symbol_table = symbol_table
+        self.current_function = None
 
     def build(self):
         self.build_translation_unit(self.tree.getChild(0))
@@ -20,9 +21,14 @@ class ASTBuilder:
             self.build_function_definition(tree.getChild(0))
 
     def build_function_definition(self, tree: grammerParser.RContext):
-        if tree.getChildCount() == 3:
-            return_type = self.build_declaration_specifiers(tree.getChild(0))
-            self.build_function_declarator(tree.getChild(1))
+        self.symbol_table.open_function_scope()
+        return_type = self.build_declaration_specifiers(tree.getChild(0))
+        function_name, parameter_type_list = self.build_function_declarator(tree.getChild(1))
+        self.current_function = function_name
+        self.symbol_table.add_function_name(function_name, return_type, len(parameter_type_list), parameter_type_list)
+        self.build_compound_statement(tree.getChild(2))
+        size = self.symbol_table.close_function_scope()
+        self.symbol_table.set_function_size(self.current_function, size)
 
     def build_declaration_specifiers(self, tree: grammerParser.RContext):
         return tree.getText()
@@ -31,12 +37,11 @@ class ASTBuilder:
         if tree.getChildCount() == 3:
             function_name = self.build_function_name(tree.getChild(0))
             return function_name, []
-        if(tree.getChildCount() == 4):
+        if tree.getChildCount() == 4:
             function_name = self.build_function_name(tree.getChild(0))
             self.current_function = function_name
             parameter_type_list = self.build_parameter_list(tree.getChild(2))
             return function_name, parameter_type_list
-
 
     def build_function_name(self, tree: grammerParser.RContext):
         if tree.getPayload().type == grammerLexer.IDENTIFIER:
@@ -55,4 +60,16 @@ class ASTBuilder:
         return parameter_type_list
 
     def build_parameter_declaration(self, tree: grammerParser.RContext):
-        pass
+        identifier_type = self.build_declaration_specifiers(tree.getChild(0))
+        identifier_name = self.build_parameter_declarator(tree.getChild(1))
+        self.symbol_table.add_variable_name(identifier_name, identifier_type)
+        return [identifier_type]
+
+    def build_parameter_declarator(self, tree: grammerParser.RContext):
+        if tree.getChildCount() != 1:
+            pass  # TODO 函数参数出错
+        else:
+            return tree.getText()
+
+    def build_compound_statement(self, tree: grammerParser.RContext):
+        print(self.symbol_table)
