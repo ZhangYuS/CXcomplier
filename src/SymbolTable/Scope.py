0f927e1c86e864207297e5fa813dcfa7a0e9ca50
SYRain <834544739@qq.com>
2019-12-06 12:11:16 +0800

    增加函数作用域

--- src/SymbolTable/Scope.py
+++ src/SymbolTable/Scope.py
@@ -1,14 +1,31 @@
+from .Symbol import Symbol
+
 class Scope:
-    father_scope = None
-    child_scopes = []
 
     def __init__(self, father_scope):
         self.father_scope = father_scope
+        self.child_scopes = []
+        self.variables = dict()
+        self.address = 0
 
     def open_scope(self):
         child_scope = Scope(self)
         self.child_scopes.append(child_scope)
+        child_scope.address = self.address
         return child_scope
+
+    def close_scope(self):
+        self.father_scope.address = self.address
+        return self.father_scope
+
+    def open_function_scope(self):
+        child_scope = Scope(self)
+        self.child_scopes.append(child_scope)
+        child_scope.address = 5
+        return child_scope
+
+    def close_function_scope(self):
+        return self.father_scope, self.address
 
     def get_father_scope(self):
         return self.father_scope
