a43672ced5c3a9d7fae1e65c9e41267e6dad2c4b
SYRain <834544739@qq.com>
2019-12-12 00:21:21 +0800

    完成 do...while 语句

--- src/ast/ASTBuilder.py
+++ src/ast/ASTBuilder.py
@@ -482,3 +482,12 @@ class ASTBuilder:
             if not isinstance(after_before, list):
                 after_before = [after_before]
             return IterationStatement(condition, f'label{self.label - 2}', f'label{self.label - 1}', [], after_before, [])
+        if tree.getChild(0).getText() == 'do':
+            condition = self.build_assignment_expression(tree.getChild(4))
+            if condition.get_type() != 'bool':
+                pass  # TODO 条件为 bool
+            after_before = self.build_statement(tree.getChild(1))
+            if not isinstance(after_before, list):
+                after_before = [after_before]
+            return IterationStatement(condition, f'label{self.label - 2}', f'label{self.label - 1}', after_before, after_before,
+                                      [])
